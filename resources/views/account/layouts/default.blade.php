c14f5219fe25b831d8a4800f4d4ab70065d18fea
Vitalii Kalaida <nevadskiy@gmail.com>
2019-01-15 22:34:53 +0200

    Add stats block

--- resources/views/account/layouts/default.blade.php
+++ resources/views/account/layouts/default.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+    @include('account.layouts._stats')
+
     <section class="section">
         <div class="container">
             <div class="columns">
